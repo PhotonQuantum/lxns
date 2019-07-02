c4c7a4ef5bb25a74f0af6fb5de09289e6ad1a55f
PhotonQuantum <cy.n01@outlook.com>
2019-07-03 02:02:41 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+import lxns
+
+
+setup(
+    name='lxns',
+    version=lxns.__version__,
+    description='A container helper for nspawn',
+    author='LightQuantum',
+    author_email='cy.n01@outlook.com',
+    packages=[
+        'lxns',
+    ],
+    data_files=[
+        ('/etc/bash_completion.d', ['extras/lxns.completion'])
+    ],
+    url='https://github.com/PhotonQuantum/lxns',
+    keywords=['container'],
+    classifiers=(
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+    ),
+    install_requires=[
+        'fire',
+        'loguru',
+        'python-slugify'
+    ],
+    entry_points={
+        'console_scripts': [
+            "lxns=lxns.__main__:main",
+        ]
+    },
+)
