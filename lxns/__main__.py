05d081a68db19506a134c2f831bdc44b5533d264
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-27 05:13:10 +0000

    Adding tarfile member sanitization to extractall()

--- lxns/__main__.py
+++ lxns/__main__.py
@@ -148,7 +148,26 @@ class entrypoint(object):
         os.makedirs(os.path.join(consts.IMAGE_DIR, name))
         print(f"Decompressing {tar_file} to image {name}")
         with tarfile.open(tar_file) as f:
-            f.extractall(os.path.join(consts.IMAGE_DIR, name))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, os.path.join(consts.IMAGE_DIR,name))
         print("Image staged.")
 
     @root
